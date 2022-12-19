baf7be36dcfad1af3e5f2bebf37d520eb3e40f29
JakesterIX <jacoblammert9@gmail.com>
2022-12-18 17:32:31 -0800

    adding

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import List from "./components/List";
+import Alert from "./components/Alert";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem("list")));
+  } else {
+    return [];
+  }
+};
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, "danger", "Please enter a movie");
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "Value Changes");
+    } else {
+      showAlert(true, "success", "Movie added to the list");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+  const removeItem = (id) => {
+    showAlert(true, "danger", "Movie removed from watchlist");
+    setList(list.filter((item) => item.id !== id));
+  };
+  const editItem = (id) => {
+    const editItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(editItem.title);
+  };
+  const clearList = () => {
+    showAlert(true, "danger", "Cleared watchlist");
+    setList([]);
+  };
+
+  return (
+    <section className="section-center">
+      <form onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3 style={{ marginBottom: "1.5rem", textAlign: "center" }}>
+          Movie Watchlist
+        </h3>
+        <div className="mb-3 form">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Movie"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <button type="submit" className="btn btn-success">
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div style={{ marginTop: "2rem" }}>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <div className="text-center">
+            <button className="btn btn-warning" onClick={clearList}>
+              Clear Movies
+            </button>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default App;
